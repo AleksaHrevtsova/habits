989cab9d661ffa406c019cd2c8ec152665a81299
renatakov <renatakov1010@gmail.com>
2020-10-31 14:02:43 +0200

    habits done

--- src/components/userprofile/index.js
+++ src/components/userprofile/index.js
@@ -1,47 +1,49 @@
-console.log(`hello`);
 // ====================== renata
 
-// let goBack = document.querySelector(".s8_like_plusik");
-// goBack.addEventListener("click", () => {
-//   window.history.go(-1);
-// });
-// console.log(goBack);
-// // ===============================================================
+let goBack = document.querySelector(".s8_like_plusik");
+goBack.addEventListener("click", () => {
+  window.history.go(-1);
+});
+console.log(goBack);
+// ===============================================================
 
-// let delButton = document.querySelector(".s8_btn_btr");
-// console.log(delButton);
+let delButton = document.querySelector(".s8_btn_btr");
+console.log(delButton);
 
-// const users1 = [{ id: "1" }];
+const users1 = [{ id: "1" }];
 
-// // создаем прослушку для кнопки удалить
-// function removeChild(id) {
-//   if(users1.filter(users1.id === id)) {
-//     users1.remove();
-//    };
-// };
-// delButton.addEventListener("click", removeChild);
+// создаем прослушку для кнопки удалить
+function removeChild(id) {
+  if(users1.filter(users1.id === id)) {
+    users1.remove();
+   };
+};
+delButton.addEventListener("click", removeChild);
 
-// // ===============================================================
+// ===============================================================
 
-// let elem = document.getElementById("myBar");
-// let startDate = Date.now();
-// console.log(startDate)
-console.log("ok")
-// class Habit  {
-//    constructor(startDate, period){
-//      this.startDate = +startDate;
-//      this.period = period;
-//      this.currentDay = 0;
-//      this._progress = 0;
-//      }
-//      toSetCurrentDay(){
-//     let todayDate = Date.now();
-//     this.currentDay = (todayDate - this.startDate)/1000/60/60/24;
-//     return this.currentDay
-//   }
+let elem = document.getElementById("myBar");
+let startDate = Date.now();
+console.log(startDate)
+class Habit  {
+   constructor(startDate, period){
+     this.startDate = +startDate;
+     this.period = period;
+     this._currentDay = 0;
+     this._progress = 0;
+     }
+     toSetCurrentDay(){
+    let todayDate = Date.now();
+    this.currentDay = (todayDate - this.startDate)/1000/60/60/24;
+    return this._currentDay
+  }
+  get currentDay(){
+    return this._currentDay
+  }
+}
+const startDay = new Date('Oct 30 2020')
 
-// }
-// const startDay = new Date('Oct 30 2020')
+const myHabit = new Habit(startDay, 21)
+console.log(myHabit)
 
-// const myHabit = new Habit(startDay, 21)
-// console.log(myHabit)
+// 
--- src/index.js
+++ src/index.js
@@ -2,4 +2,3 @@ import css from "./css/style.css";
 
 import './components/auth'
 import './components/userprofile'
-console.log(`hello`);
