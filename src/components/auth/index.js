15b45023f744d8c503049459b6156105531096f0
dan4ik58620 <danyakotkot@gmail.com>
2020-11-07 12:22:08 +0200

    монтажная область 2

---
+++ src/components/auth/index.js
@@ -0,0 +1,12 @@
+
+import css from "./css/auth.css"
+import "./components/auth/index"
+
+var name = document.getElementById("login");
+var surname = document.getElementById("surname");
+var vxd = document.getElementById("vxod");
+var check = document.getElementById("remember");
+vxd.onclick = function()
+{
+	alert('ok');
+}
