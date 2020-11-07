1d3a1bc0bde773ca82ec48e208c78bda4aa70121
dan4ik58620 <danyakotkot@gmail.com>
2020-11-07 12:58:08 +0200

    монтажная-область2 2.0

--- src/components/auth/index.js
+++ src/components/auth/index.js
@@ -1,12 +1,3 @@
 
-import css from "./css/auth.css"
-import "./components/auth/index"
-
-var name = document.getElementById("login");
-var surname = document.getElementById("surname");
-var vxd = document.getElementById("vxod");
-var check = document.getElementById("remember");
-vxd.onclick = function()
-{
-	alert('ok');
-}
+// import css from "../../css/auth.css"
+// import "./components/auth/index"
