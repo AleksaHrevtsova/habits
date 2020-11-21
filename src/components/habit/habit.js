bbf9e3fcc5933d6e524edbb6d95cba6a10b20ed9
Vanya <56477758+Vanya1808@users.noreply.github.com>
2020-11-21 11:06:19 +0300

    2

--- src/components/habit/habit.js
+++ src/components/habit/habit.js
@@ -1,9 +1,9 @@
 var d = new Date();
 
-let date1 = document.getElementById('date1');
-let date2 = document.getElementById('date2');
-let date3 = document.getElementById('date3');
-let date4 = document.getElementById('date4');
+let date1 = document.getElementById("date1");
+let date2 = document.getElementById("date2");
+let date3 = document.getElementById("date3");
+let date4 = document.getElementById("date4");
 
 let date1V = d.getDay() - 3;
 let date2V = d.getDay() - 2;
@@ -11,14 +11,14 @@ let date3V = d.getDay() - 1;
 let date4V = d.getDay();
 
 const dates = {
-  1: 'пн',
-  2: 'вт',
-  3: 'ср',
-  4: 'чт',
-  5: 'пт',
-  6: 'сб',
-  7: 'вс'
-}
+  1: "пн",
+  2: "вт",
+  3: "ср",
+  4: "чт",
+  5: "пт",
+  6: "сб",
+  7: "вс",
+};
 
 date1V = dates[date1V];
 date2V = dates[date2V];
@@ -30,10 +30,10 @@ date2.textContent = date2V;
 date3.textContent = date3V;
 date4.textContent = date4V;
 
-let date1С = document.getElementById('date1С');
-let date2С = document.getElementById('date2С');
-let date3С = document.getElementById('date3С');
-let date4С = document.getElementById('date4С');
+let date1С = document.getElementById("date1С");
+let date2С = document.getElementById("date2С");
+let date3С = document.getElementById("date3С");
+let date4С = document.getElementById("date4С");
 
 let date1VС = d.getUTCDate() - 3;
 let date2VС = d.getUTCDate() - 2;
@@ -45,3 +45,19 @@ date2C.textContent = date2VС;
 date3C.textContent = date3VС;
 date4C.textContent = date4VС;
 
+function createHabit(e) {
+  e.preventDefault();
+  const data = {
+    habitNameD: habitName.value,
+  };
+  insertElement(habitTemplate, data, habits);
+  cntunderModal.style.display = "contents";
+  closeDivs.style.display = "none";
+}
+
+submit.addEventListener("click", createHabit);
+
+function insertElement(template, data, place) {
+  const elem = template(data);
+  place.insertAdjacentHTML("beforeend", elem);
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 import css from "./css/style.css";
-import habit from "./components/habit/habit.js" ;
+import habit from "./components/habit/habit.js";
+import habitTemplate from "./templates/template.hbs";
+
+function createHabit(e) {
+    e.preventDefault();
+    const data = {
+      habitNameD: habitName.value,
+    };
+    insertElement(habitTemplate, data, habits);
+    cntunderModal.style.display = "contents";
+    closeDivs.style.display = "none";
+  }
+  
+  submit.addEventListener("click", createHabit);
+  
+  function insertElement(template, data, place) {
+    const elem = template(data);
+    place.insertAdjacentHTML("beforeend", elem);
+  }
+  
